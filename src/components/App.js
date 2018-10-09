8b8332ab39456480659178011def02da1d27cb6f
Dawid Ceynowa <dejw.ce@gmail.com>
2018-10-10 00:11:23 +0200

    add simple counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
       <div>
-        CLEAN :)
+        <Counter/>
       </div>
     );
   }
