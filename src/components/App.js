41cd17cd22cbc5ec1517b203cd2cd6a8a3628e15
Dawid Ceynowa <dejw.ce@gmail.com>
2018-10-10 22:07:05 +0200

    create input which allow to add new tasks

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import './App.css';
 import ToDoList from './ToDoList';
 
 class App extends Component {
+  
+  myTasks = [
+    'Study React',
+    'Go for a walk'
+  ]
+  
   render() {
     return (
       <div>
-        <ToDoList title="My stuff" />
+        <ToDoList title='My stuff' tasks={this.myTasks}/>
       </div>
     );
   }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
 class ToDoList extends React.Component {
+    
+    state = {
+        tasks: this.props.tasks,
+        draft: ''
+    }
+
+    updateDraft = event => {
+        this.setState({draft: event.target.value})
+    }
+
+    addTask = () => {
+        const list = this.state.tasks;
+        list.push(this.state.draft);
+        this.setState({tasks: list, draft: ''})
+    }
+    
     render () {
         return (
             <div>
                 <h1>
                     {this.props.title}
                 </h1>
+                
+                {this.state.tasks.map(task => <div><p> {task} </p></div>)}
+
+                <input type="text" onChange={this.updateDraft} value={this.state.draft}/>
+                <button onClick={this.addTask}> Add </button>
             </div>
         )
     }
